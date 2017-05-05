e948782ff2d7549f523c4b3d0e7263caccba3223
Ian Beck <ian@onecrayon.com>
2017-05-04 23:02:28 -0700

    Added very basic badge and username generation upon User creation

--- application/models/user.py
+++ application/models/user.py
@@ -1,7 +1,25 @@
 from datetime import datetime
+from random import choice
+import string
 
 from flask_login.mixins import UserMixin
-from application import db
+from application import db, bcrypt
+
+
+def generate_badges(number=8, length=4):
+    return [''.join([
+        # First character is always a number
+        choice(string.digits),
+        # Next characters are alphanumeric or middle punctuation
+        ''.join(choice(string.ascii_lowercase + string.digits + '*&-+=') for _ in range(length-2)),
+        # Final character alphanumeric or ending punctuation
+        choice(string.ascii_lowercase + string.digits + '*%!?')
+    ]) for _ in range(number)]
+
+
+def kid_friendly(badge):
+    # TODO: actually test against a blacklist for non-kid-friendly words
+    return True
 
 
 class User(db.Model, UserMixin):
@@ -13,3 +31,33 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(255), nullable=False)
     created = db.Column(db.DateTime, default=datetime.utcnow)
     modified = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    def __init__(self, email, password, badge=None, username=None):
+        self.email = email
+        self.password = bcrypt.generate_password_hash(password)
+        self.badge = badge if badge else User.fetch_badges(True)
+        self.username = username if username else choice([
+            'Aradel Summergaard', 'Brennen Blackcloud', 'Coal Roarkwin',
+            'Dimona Odinstar', 'Jessa Na Ni', 'Leo Sunshadow',
+            'Lulu Firststone', 'Maeoni Viper', 'Namine Hymntide',
+            'Noah Redmoon', 'Odette Diamondcrest', 'Orrick Gilstream',
+            'Rin Northfell', 'Saria Guideman', 'Victoria Glassfire'
+        ])
+    
+    @staticmethod
+    def fetch_badges(single=False, maximum=8, length=4):
+        options = generate_badges(number=maximum, length=length)
+        # Test for kid-friendliness
+        options = [x for x in options if kid_friendly(x)]
+        # Highly unlikely, but if *all* options were bad, try again
+        if not options:
+            return User.fetch_badges(single, maximum, length)
+        taken = [
+            badge for (badge,) in db.session.query(User.badge).filter(User.badge.in_(options)).all()
+        ]
+        if taken:
+            options = [x for x in options if x not in taken]
+        # Highly unlikely, but if all random badges are taken, try again
+        if not options:
+            return User.fetch_badges(single, maximum, length)
+        return options[0] if single else options
