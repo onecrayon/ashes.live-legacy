642e8cb45931e516d37eef7da4edf1ddf2968093
Ian Beck <ian@onecrayon.com>
2017-07-15 22:27:15 -0700

    Added 404 template handling

---
+++ application/error_handlers.py
@@ -0,0 +1,10 @@
+from flask import Blueprint, render_template
+
+from application import app
+
+mod = Blueprint('error_handlers', __name__)
+
+
+@mod.app_errorhandler(404)
+def not_found(error):
+    return render_template('404.html'), 404
--- manager.py
+++ manager.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 
-from application import app
-from application.views import decks as public_decks, home as public_home, player as public_player
+from application import app, error_handlers
+from application.views import (
+	decks as public_decks, home as public_home, player as public_player
+)
 
 
+app.register_blueprint(error_handlers.mod)
 app.register_blueprint(public_decks.mod)
 app.register_blueprint(public_home.mod)
 app.register_blueprint(public_player.mod)
