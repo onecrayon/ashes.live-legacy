a56f7c7d1eafbc7b684ef9381894225648cec279
Ian Beck <ian@onecrayon.com>
2017-07-23 10:48:00 -0700

    Tweaked webpack logic to allow easier addition of new entry points

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,10 @@ var path = require('path')
 
 module.exports = {
 	entry: {
-		deckbuilder: './app/static/src/app.js'
+		app: './app/static/src/app.js'
 	},
 	output: {
-		filename: 'app.js',
+		filename: '[name].js',
 		path: path.resolve(__dirname, 'app/static/js')
 	},
 	module: {
