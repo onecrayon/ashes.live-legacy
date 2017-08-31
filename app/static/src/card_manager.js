41b96f4af377f112f736d67299b33ea112609c0f
Ian Beck <ian@onecrayon.com>
2017-08-31 15:45:38 -0700

    Stubbed out more CardManager functions

--- app/static/src/card_manager.js
+++ app/static/src/card_manager.js
@@ -5,6 +5,17 @@
 export default class {
 	constructor () {
 		// TODO: loop over globals.cards and globals.dice to construct indices necessary for easily relating the two?
+		// Create lookup table by ID
+		this.idMap = {}
+		for (let card in globals.cardData) {
+			this.idMap[card.id] = card
+		}
+	}
+	cardById (id) {
+		return this.idMap[id] || null
+	}
+	cardsByType (type) {
+		// TODO: return array of cards based on the given type and active sorting
 	}
 	// TODO: add methods for fetching, filtering, and sorting card and die data
 }
