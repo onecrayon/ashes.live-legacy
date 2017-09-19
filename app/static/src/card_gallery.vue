3a8556c00335b2ef873cfc13a08bb767eed1f14a
Ian Beck <ian@onecrayon.com>
2017-09-18 22:18:12 -0700

    Added divine/sympathy buttons, but disabled based on if the set is included in listing or not

--- app/static/src/card_gallery.vue
+++ app/static/src/card_gallery.vue
@@ -3,7 +3,8 @@
 		<div v-if="phoenixborn" class="gallery">
 			<div class="filters">
 				<div class="btn-group">
-					<button v-on:click="toggleDiceLogic" class="btn">{{ diceLogicText }}:</button
+					<button v-on:click="toggleDiceLogic"
+						class="btn btn-all" :class="{active: diceLogicActive }">{{ diceLogicText }}:</button
 					><button v-on:click="toggleDie('ceremonial')"
 						class="btn phg-ceremonial-power" :class="{active: ceremonialActive }"
 						title="Ceremonial"></button
@@ -16,12 +17,12 @@
 					><button v-on:click="toggleDie('natural')"
 						class="btn phg-natural-power" :class="{active: naturalActive }"
 						title="Natural"></button
-					><!--<button v-on:click="toggleDie('divine')"
-						class="btn phg-divine-power" :class="{active: divineActive }"
-						title="Divine"></button
+					><button v-on:click="toggleDie('divine')"
+						class="btn phg-divine-power" :class="{active: divineActive }" :disabled="set5Disabled"
+						title="Divine">D</button
 					><button v-on:click="toggleDie('sympathy')"
-						class="btn phg-sympathy-power" :class="{active: sympathyActive }"
-						title="Sympathy"></button>-->
+						class="btn phg-sympathy-power" :class="{active: sympathyActive }" :disabled="set6Disabled"
+						title="Sympathy">S</button>
 				</div>
 			</div>
 			<ul class="listing">
@@ -70,12 +71,15 @@
 			diceLogicText () {
 				return this.$store.state.filters.diceLogic == 'or' ? 'Any' : 'All'
 			},
+			diceLogicActive () { return this.$store.state.filters.diceLogic == 'and' },
 			ceremonialActive () { return includes(this.$store.state.filters.dice || [], 'ceremonial') },
 			charmActive () { return includes(this.$store.state.filters.dice || [], 'charm') },
 			illusionActive () { return includes(this.$store.state.filters.dice || [], 'illusion') },
 			naturalActive () { return includes(this.$store.state.filters.dice || [], 'natural') },
 			divineActive () { return includes(this.$store.state.filters.dice || [], 'divine') },
-			sympathyActive () { return includes(this.$store.state.filters.dice || [], 'sympathy') }
+			sympathyActive () { return includes(this.$store.state.filters.dice || [], 'sympathy') },
+			set5Disabled () { return !includes(this.$store.state.filters.releases, 5) },
+			set6Disabled () { return !includes(this.$store.state.filters.releases, 6) }
 		},
 		methods: {
 			toggleDiceLogic () {
