44a902ebe3837dc98b79640c5e6d250f8274abda
Ian Beck <ian@onecrayon.com>
2017-09-23 17:17:16 -0700

    Implemented bindings to handle search logic

--- app/static/src/filters/text.vue
+++ app/static/src/filters/text.vue
@@ -1,6 +1,9 @@
-<<template>
-	<div class="form-field">
-		<input v-on:input="triggerFilter" type="text" placeholder="Filter by name or text...">
+<template>
+	<div class="input-group">
+		<div class="form-field">
+			<input v-model="search" type="text" placeholder="Filter by name or text...">
+		</div>
+		<button v-on:click="clearSearch" :disabled="!search" class="btn">Clear</button>
 	</div>
 </template>
 
@@ -8,12 +11,22 @@
 	import {debounce} from 'lodash'
 
 	export default {
+		computed: {
+			search: {
+				get () {
+					return this.$store.state.filters.search
+				},
+				set: debounce(function (value) {
+					let search = value || null
+					this.$store.commit('setSearch', search)
+					this.$store.commit('filterCards')
+				}, 400)
+			}
+		},
 		methods: {
-			triggerFilter: debounce(function (event) {
-				let search = event.target.value || null
-				this.$store.commit('setSearch', search)
-				this.$store.commit('filterCards')
-			}, 400)
+			clearSearch () {
+				this.search = null
+			}
 		}
 	}
 </script>
