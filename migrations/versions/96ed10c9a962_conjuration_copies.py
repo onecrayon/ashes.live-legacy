a1a08a7f5a01358931eba2cf9a10c525300d98dc
Ian Beck <ian@onecrayon.com>
2017-10-01 23:41:48 -0700

    Added copies tracking to main SQL database for conjurations
    This is necessary for quickly fetching card data for the My Decks listing, for instance.

---
+++ migrations/versions/96ed10c9a962_conjuration_copies.py
@@ -0,0 +1,45 @@
+"""Added conjuration copies column
+
+Revision ID: 96ed10c9a962
+Revises: 8efcb18f36a7
+Create Date: 2017-10-01 22:40:51.930121
+
+"""
+import json
+
+from alembic import op
+import sqlalchemy as sa
+
+from app import db
+from app.models.card import Card
+
+
+# revision identifiers, used by Alembic.
+revision = '96ed10c9a962'
+down_revision = '8efcb18f36a7'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.add_column('card', sa.Column('copies', sa.SmallInteger(), nullable=True))
+
+    cards = Card.query.filter(
+        Card.card_type.in_(['Conjuration', 'Conjured Alteration Spell'])
+    ).all()
+    for card in cards:
+        card_json = json.loads(card.json)
+        if card.stub == 'masked-wolf':
+            # Masked Wolf is missing card copies (oversight from original import)
+            card_json['copies'] = '5'
+            card.json = json.dumps(card_json)
+        elif card.stub == 'winged-lioness':
+            # And so is Winged Lioness...
+            card_json['copies'] = '4'
+            card.json = json.dumps(card_json)
+        card.copies = int(card_json['copies'])
+    db.session.commit()
+
+
+def downgrade():
+    op.drop_column('card', 'copies')
