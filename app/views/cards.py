3c0f69ec69ecf69733fab588db8d44df63a8531d
Ian Beck <ian@onecrayon.com>
2018-01-30 13:54:02 -0800

    Added card detail page
    * Calculate the number of users and decks associated with a particular card
    * Show full card text (as text)
    * List all related conjurations (and root summoning card)

--- app/views/cards.py
+++ app/views/cards.py
@@ -1,8 +1,21 @@
 """Card gallery"""
 
-from flask import Blueprint, render_template
+import json
+
+from flask import abort, current_app, Blueprint, render_template
+
+from app import db
+from app.models.card import Card
+from app.models.deck import Deck, DeckCard
 
 mod = Blueprint('cards', __name__, url_prefix='/cards')
+
+
+def gather_conjurations(card):
+    conjurations = card.conjurations if card.conjurations else []
+    for conjuration in conjurations:
+        conjurations = conjurations + gather_conjurations(conjuration)
+    return conjurations
 
 
 @mod.route('/')
@@ -14,4 +27,50 @@ def index():
 @mod.route('/<stub>/')
 def detail(stub):
     """Card details"""
-    return render_template('wip.html')
+    card = Card.query.options(
+        db.joinedload('conjurations')
+    ).filter(Card.stub == stub).first()
+    if not card:
+        abort(404)
+    # Gather up all related conjurations
+    root_card = card if not card.summon_id else None
+    if card.summon_id:
+        summon_card = Card.query.get(card.summon_id)
+        while summon_card:
+            if not summon_card.summon_id:
+                root_card = summon_card
+                break
+            summon_card = Card.query.get(summon_card.summon_id)
+    conjurations = gather_conjurations(root_card) if root_card.conjurations else []
+    # Gather stats
+    if root_card.card_type == 'Phoenixborn':
+        query = db.session.query(
+            db.func.count(Deck.id).label('decks'),
+            db.func.count(db.func.distinct(Deck.user_id)).label('users')
+        ).filter(
+            Deck.phoenixborn_id == root_card.id
+        )
+    else:
+        query = db.session.query(
+            db.func.count(DeckCard.deck_id).label('decks'),
+            db.func.count(db.func.distinct(Deck.user_id)).label('users')
+        ).join(
+            Deck, Deck.id == DeckCard.deck_id
+        ).filter(
+            DeckCard.card_id == root_card.id
+        )
+    counts = query.filter(
+        Deck.is_snapshot.is_(False)
+    ).first()
+    dice = Card.flags_to_dice(card.dice_flags)
+    dice.remove('basic')
+    return render_template(
+        'cards/detail.html',
+        card=json.loads(card.json),
+        root_card=root_card,
+        conjurations=conjurations,
+        dice=dice,
+        release=current_app.config['RELEASE_NAMES'][card.release],
+        decks_count=counts.decks,
+        users_count=counts.users
+    )
