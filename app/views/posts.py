c728b83e93e30c6b5dc9ef6587faf23f1014568e
Ian Beck <ian@onecrayon.com>
2018-05-06 18:31:30 -0700

    Added basic section listing for posts index
    This page is not currently linked from anywhere, so no need for anything beyond the basics.

--- app/views/posts.py
+++ app/views/posts.py
@@ -34,7 +34,7 @@ def get_section_choices():
 @mod.route('/')
 def index():
     """List all available sections"""
-    pass
+    return render_template('posts/index.html', sections=db.session.query(Section).all())
 
 
 @mod.route('/<stub>/')
