7ce87cf06158aa0e4eff57b5c78a481aceea998d
Ian Beck <ian@onecrayon.com>
2018-05-25 15:01:30 -0700

    Moved quantity button comparisons to computed properties
    Methods are called every time the view is re-rendered, so this should speed things up.

--- app/static/src/app/gallery/listing/qty_buttons.vue
+++ app/static/src/app/gallery/listing/qty_buttons.vue
@@ -1,16 +1,16 @@
 <template>
 	<div class="btn-group">
 		<button class="btn btn-qty"
-			:class="[isQtyActive(card.id, 0) ? 'active' : '', classes]"
+			:class="[isQty0Active ? 'active' : '', classes]"
 			@click="setCardQty(card.id, 0)" v-html="zeroOutput || 0"></button
 		><button class="btn btn-qty"
-			:class="[isQtyActive(card.id, 1) ? 'active' : '', classes]"
+			:class="[isQty1Active ? 'active' : '', classes]"
 			@click="setCardQty(card.id, 1)">1</button
 		><button class="btn btn-qty"
-			:class="[isQtyActive(card.id, 2) ? 'active' : '', classes]"
+			:class="[isQty2Active ? 'active' : '', classes]"
 			@click="setCardQty(card.id, 2)">2</button
 		><button class="btn btn-qty"
-			:class="[isQtyActive(card.id, 3) ? 'active' : '', classes]"
+			:class="[isQty3Active ? 'active' : '', classes]"
 			@click="setCardQty(card.id, 3)">3</button>
 	</div>
 </template>
@@ -18,13 +18,26 @@
 <script>
 	export default {
 		props: ['card', 'classes', 'zeroOutput'],
+		computed: {
+			isQty0Active () {
+				return !this.$store.state.deck.cards[this.card.id]
+			},
+			isQty1Active () {
+				const count = this.$store.state.deck.cards[this.card.id]
+				return count && count === 1
+			},
+			isQty2Active () {
+				const count = this.$store.state.deck.cards[this.card.id]
+				return count && count === 2
+			},
+			isQty3Active () {
+				const count = this.$store.state.deck.cards[this.card.id]
+				return count && count === 3
+			},
+		},
 		methods: {
 			setCardQty (id, qty) {
 				this.$store.commit('setCardQty', {id: id, qty: qty})
-			},
-			isQtyActive (id, qty) {
-				return (!this.$store.state.deck.cards[id] && qty === 0) ||
-					this.$store.state.deck.cards[id] === qty
 			}
 		}
 	}
