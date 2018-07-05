b4a475f77fc97002597b22a73d90e6253260d12e
Ian Beck <ian@onecrayon.com>
2018-07-05 16:19:15 -0700

    Added new expansion information to relevant config variables

--- config/config.py
+++ config/config.py
@@ -19,6 +19,8 @@ RELEASE_NAMES = {
     8: 'The Path of Assassins',
     9: 'The Goddess of Ishra',
     10: 'The Boy Among Wolves',
+    11: 'The Demons of Darmas',
+    12: 'The Spirits of Memoria',
     101: 'Dimona Odinstar (promo)',
     102: 'Lulu Firststone (promo)',
     103: 'Orrick Gilstream (promo)'
