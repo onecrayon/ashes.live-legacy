b4de2f60c7fd0092bd6190fb60587ecc5d5c4ec0
Ian Beck <ian@onecrayon.com>
2019-11-18 21:13:53 -0800

    Highlight unowned cards when filtering by personal collection

--- app/static/src/app/components/card_link.vue
+++ app/static/src/app/components/card_link.vue
@@ -1,5 +1,5 @@
 <template>
-	<a :href="href" class="card" :target="target"><slot>{{ card.name }}</slot></a>
+	<a :href="href" class="card" :class="{'not-owned': isNotOwned}" :target="target"><slot>{{ card.name }}</slot></a>
 </template>
 
 <script>
@@ -21,6 +21,9 @@
 			},
 			target () {
 				return globals.galleryOnly ? false : '_blank'
+			},
+			isNotOwned () {
+				return this.$store.state.options.releases === 'mine' && this.$store.state.options.userCollection && !this.$store.state.options.userCollection.includes(this.card.release.id)
 			}
 		}
 	}
